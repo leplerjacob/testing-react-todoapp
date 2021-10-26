e067320746cb5b8f2c90965feda108f862d12abd
Jacob Lepler <lepler.jacob@gmail.com>
2021-10-25 23:04:13 -0400

    Create helper function file

---
+++ src/helpers/Helpers.js
@@ -0,0 +1,10 @@
+const initialTodos = [
+    { id: 1, todo: "Walk the dog", date: "2021-10-20" },
+    { id: 2, todo: "Visit grandparents", date: "2021-10-22" },
+    { id: 3, todo: "Attend React conference", date: "2021-10-26" },
+  ];
+
+// Organizes ids in ascending order by date
+export function orderIDs(todos){
+    
+}
