cd06ecb5d7ee755c52a67d50892d8333a8f89152
Jacob Lepler <lepler.jacob@gmail.com>
2021-10-25 23:02:05 -0400

    Create app and import todo form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import AddTodo from "./components/addTodo/AddTodo";
+
+const initialTodos = [
+  { id: 1, todo: "Walk the dog", date: "2021-10-20" },
+  { id: 2, todo: "Visit grandparents", date: "2021-10-22" },
+  { id: 3, todo: "Attend React conference", date: "2021-10-26" },
+];
 
 function App() {
+  const [todos, setTodos] = useState(initialTodos);
+
+  function addTodo(todo) {
+    console.log(todo);
+    // ...adds todo to todos list state object
+    // organize id's
+    // setTodos([...initialTodos, todo])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="header welcome">Todo Application</h1>
+      <AddTodo addTodo={addTodo} />
     </div>
   );
 }
