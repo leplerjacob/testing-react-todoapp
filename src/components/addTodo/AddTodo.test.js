008df7bafcbcfd6afc4477a296ce033ce4281b29
Jacob Lepler <lepler.jacob@gmail.com>
2021-10-26 00:12:49 -0400

    Add jest test for AddTodo component

---
+++ src/components/addTodo/AddTodo.test.js
@@ -0,0 +1,15 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import AddTodo from "./AddTodo";
+
+test("App renders todo form", () => {
+  render;
+
+  expect();
+});
+
+test("Default form for date is current date", () => {});
+
+test("Updates value of input on change", () => {});
+
+test("Form resets input values on submit", () => {});
