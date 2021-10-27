83463e14cd3d99a8e17ad529dc0e1fe30d4f6057
Jacob Lepler <lepler.jacob@gmail.com>
2021-10-26 22:11:05 -0400

    Add AddTodo test file with snapshot

--- src/components/addTodo/AddTodo.test.js
+++ src/components/addTodo/AddTodo.test.js
@@ -2,14 +2,15 @@ import React from "react";
 import renderer from "react-test-renderer";
 import AddTodo from "./AddTodo";
 
+// 'jest -u' updates snapshot
 test("App renders todo form", () => {
-  render;
+  const tree = renderer.create(<AddTodo />).toJSON();
 
-  expect();
+  expect(tree).toMatchSnapshot();
 });
 
 test("Default form for date is current date", () => {});
 
-test("Updates value of input on change", () => {});
+// test("Updates value of input on change", () => {});
 
-test("Form resets input values on submit", () => {});
+// test("Form resets input values on submit", () => {});
